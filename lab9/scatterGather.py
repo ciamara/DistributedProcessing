30cacea3d0aea1c6ba0d75f42b04cbaa9b29ea5d
ciamara <mruktamara64@gmail.com>
2025-05-15 16:27:07 +0200

    python multiprocessing

--- lab9/scatterGather.py
+++ lab9/scatterGather.py
@@ -1,90 +1,62 @@
-import os
 import sys
 import time
 import cv2
 import numpy as np
-import pickle
-import warnings
+import multiprocessing as mp
+from PIL import Image
 
-warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 # slicing image into *parts* horizontal lines
 def scatter(image, parts):
-    print("Slicing the image into horizontal parts")
-    h, w, _ = image.shape
-    step = h // parts
-    return [image[i*step:(i+1)*step if i != parts-1 else h, :] for i in range(parts)]
+    print("Scattering image...")
+    slices = np.array_split(image, parts, axis=0)
+    return slices
 
 # sticking slices back together
 def gather(slices):
-    print("Sticking the converted parts back together")
+    print("Gathering image...")
     return np.vstack(slices)
 
-# processes & converting to negative
-def fork_processes(slices):
+# convert single slice to negative
+def convert_slice(slice):
+    neg = 255 - slice
+    return neg
 
-    print("Converting slices to negative simultaneusly")
+# multiprocessing
+def convert(image, parts):
+    print("Converting slices into negatives...")
 
-    children = []
-    pipes = []
+    slices = scatter(image, parts)
 
-    for i, sl in enumerate(slices):
-        r, w = os.pipe()
-        pid = os.fork()
-        if pid == 0:
-            # child process (converting to negative)
-            os.close(r) 
-            sl_neg = 255 - sl
-            data = pickle.dumps(sl_neg)
-            os.write(w, data)
-            os.close(w)
-            os._exit(0)
-        else:
-            # parent process
-            os.close(w)
-            children.append(pid)
-            pipes.append(r)
+    with mp.Pool(parts) as pool:
+        neg = pool.map(convert_slice, slices)
 
-    # converted slices from children
-    results = []
-    for r in pipes:
-        data = b''
-        while True:
-            chunk = os.read(r, 4096)
-            if not chunk:
-                break
-            data += chunk
-        results.append(pickle.loads(data))
-        os.close(r)
+    result = gather(neg)
 
-    # wait for all children to finish
-    for pid in children:
-        os.waitpid(pid, 0)
-
-    return results
+    return result
 
 def main():
     
     if len(sys.argv) < 2:
-        print("Usage: python3 program.py <number_of_processes>")
+        print("Usage: python scatterGather.py <number_of_processes>")
         return
 
     parts = int(sys.argv[1])
-    image = cv2.imread('BIGobszar.jpeg')
-    cv2.imshow("original", image)
 
+    image = Image.open('mystical.jpg').convert("RGB")
+    imageArray = np.array(image)
+    
     start = time.time()
 
-    slices = scatter(image, parts)
-    converted = fork_processes(slices)
-    full = gather(converted)
+    negative = convert(imageArray, parts)
 
     end = time.time()
     print(f"Execution time: {end - start:.4f} seconds")
 
-    cv2.imshow("Negative Combined", full)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    result = Image.fromarray(negative.astype(np.uint8))
+
+    print("Saving image...")
+    result.save('neg.jpg')
 
     sys.exit(0)
 
